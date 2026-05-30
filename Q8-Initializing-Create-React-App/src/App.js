32a8664422c036ef4bc634802bb46ca3b320b01b
Sharath <sharath03.tech@gmail.com>
2026-05-31 01:34:10 +0530

    Question8-React

---
+++ Q8-Initializing-Create-React-App/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Write the message here. */}
+      <p>Welcome to React Course on Coding Ninjas.com</p>
+    </div>
+  );
+}
+
+export default App;
