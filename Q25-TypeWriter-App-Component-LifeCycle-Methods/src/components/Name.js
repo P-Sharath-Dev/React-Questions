803323ae99b78c6c944bc7e9637ad7ffa6309912
Sharath <sharath03.tech@gmail.com>
2026-08-21 16:49:13 +0530

    solved Q25-TypeWriter-App-Component-LifeCycle-Methods

---
+++ Q25-TypeWriter-App-Component-LifeCycle-Methods/src/components/Name.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+export default class Name extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      fullName: "Coding Ninjas",
+      curIndex: 0,
+      currentName: "",
+    };
+  }
+
+  // This function adds a character to the string.
+  typeWriterEffect = () => {
+    this.setState((prevState) => {
+      return {
+        curIndex: prevState.curIndex + 1,
+        currentName: prevState.fullName.substring(0, prevState.curIndex),
+      };
+    });
+  };
+
+  // Required lifecycle methods here
+  componentDidMount() {
+    if (this.props.showName) {
+      this.interval = setInterval(this.typeWriterEffect, 500);
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.showName !== this.props.showName) {
+      if (this.props.showName) {
+        this.interval = setInterval(this.typeWriterEffect, 500);
+      } else {
+        clearInterval(this.interval);
+      }
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return <h1>{this.state.currentName}</h1>;
+  }
+}
